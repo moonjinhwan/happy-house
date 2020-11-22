e8ffc44f6b7b6638389d206aca5b88eacec0bd74
moonjinhwan <moonjinh123@naver.com>
2020-11-22 21:20:56 +0900

    detail success

--- member-vue/src/components/AptDetail.vue
+++ member-vue/src/components/AptDetail.vue
@@ -4,17 +4,21 @@
       <thead>
         <tr>
           <th scope="col">번호</th>
+          <th scope="col">동</th>
           <th scope="col">아파트</th>
           <th scope="col">거래금액</th>
-          <th scope="col">건축년도</th>
+          <th scope="col">면적</th>
+          <th scope="col">거래년도</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(info, index) in getAptDetail" :key="index">
-          <th scope="row">{{ index }}</th>
-          <td>{{ info.아파트 }}</td>
-          <td>{{ info.거래금액 }}</td>
-          <td>{{ info.건축년도 }}</td>
+          <th scope="row">{{ index + 1 }}</th>
+          <td>{{ info.dong }}</td>
+          <td>{{ info.AptName }}</td>
+          <td>{{ info.dealAmount }}</td>
+          <td>{{ info.area }}</td>
+          <td>{{ info.dealYear }}</td>
         </tr>
       </tbody>
     </table>
@@ -23,6 +27,7 @@
 
 <script>
 import { mapGetters } from 'vuex';
+
 export default {
   computed: {
     ...mapGetters(['getAptDetail']),
--- member-vue/src/components/AptListItem.vue
+++ member-vue/src/components/AptListItem.vue
@@ -1,6 +1,5 @@
 <template>
   <tr @click="aptDetail">
-    <td>{{ aptInfo.no }}</td>
     <td>{{ aptInfo.dong }}</td>
     <td>{{ aptInfo.aptName }}</td>
     <td>{{ aptInfo.jibun }}</td>
@@ -10,6 +9,7 @@
 <script>
 import axios from 'axios';
 import { mapMutations } from 'vuex';
+const SERVER_URL = process.env.VUE_APP_SERVER_URL;
 export default {
   data() {
     return {};
@@ -23,19 +23,23 @@ export default {
     }),
 
     aptDetail: function() {
-      const API_KEY =
-        '9Xo0vlglWcOBGUDxH8PPbuKnlBwbWU6aO7%2Bk3FV4baF9GXok1yxIEF%2BIwr2%2B%2F%2F4oVLT8bekKU%2Bk9ztkJO0wsBw%3D%3D';
-      const API_URL =
-        'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev?LAWD_CD=' +
-        this.aptInfo.code +
-        '&DEAL_YMD=202010&serviceKey=' +
-        API_KEY;
+      // const API_KEY =
+      //   '9Xo0vlglWcOBGUDxH8PPbuKnlBwbWU6aO7%2Bk3FV4baF9GXok1yxIEF%2BIwr2%2B%2F%2F4oVLT8bekKU%2Bk9ztkJO0wsBw%3D%3D';
+      // const API_URL =
+      //   'http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev?LAWD_CD=' +
+      //   this.aptInfo.code +
+      //   '&DEAL_YMD=202010&serviceKey=' +
+      //   API_KEY;
 
       axios
-        .get(API_URL)
+        .post(`${SERVER_URL}/map/detail`, {
+          code: this.aptInfo.code,
+          aptName: this.aptInfo.aptName,
+        })
         .then((response) => {
           console.log(response);
-          this.setDetail(response.data.response.body.items.item);
+          // this.setDetail(response.data.response.body.items.item);
+          this.setDetail(response.data.detailInfo);
         })
         .catch((error) => {
           console.log(error);
