6664977e7d045bc9f0e9d61ad746f8ad3b91b8c8
skim817 <skim817@aucklanduni.ac.nz>
2020-11-25 00:57:45 +0900

    HI MJH HEALTHY BOYY

---
+++ VueSpringBootLogin/src/main/java/com/ssafy/vue/controller/BoardController.java
@@ -0,0 +1,81 @@
+package com.ssafy.vue.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.vue.model.Board;
+import com.ssafy.vue.model.service.BoardService;
+
+
+//http://localhost:9999/vue/swagger-ui.html
+@CrossOrigin(origins = { "*" }, maxAge = 6000)
+@RestController
+@RequestMapping("/api/board")
+public class BoardController {
+
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	private static final String SUCCESS = "success";
+	private static final String FAIL = "fail";
+
+	@Autowired
+	private BoardService boardService;
+
+
+	@GetMapping
+	public ResponseEntity<List<Board>> retrieveBoard() throws Exception {
+		logger.debug("retrieveBoard - 호출");
+		return new ResponseEntity<List<Board>>(boardService.retrieveBoard(), HttpStatus.OK);
+	}
+
+ 
+	@GetMapping("{no}")
+	public ResponseEntity<Board> detailBoard(@PathVariable int no) {
+		logger.debug("detailBoard - 호출");
+		return new ResponseEntity<Board>(boardService.detailBoard(no), HttpStatus.OK);
+	}
+
+
+	@PostMapping
+	public ResponseEntity<String> writeBoard(@RequestBody Board board) {
+		logger.debug("writeBoard - 호출");
+		if (boardService.writeBoard(board)) {
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+
+	@PutMapping("{no}")
+	public ResponseEntity<String> updateBoard(@RequestBody Board board) {
+		logger.debug("updateBoard - 호출");
+		logger.debug("" + board);
+		
+		if (boardService.updateBoard(board)) {
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+
+
+	@DeleteMapping("{no}")
+	public ResponseEntity<String> deleteBoard(@PathVariable int no) {
+		logger.debug("deleteBoard - 호출");
+		if (boardService.deleteBoard(no)) {
+			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
+		}
+		return new ResponseEntity<String>(FAIL, HttpStatus.NO_CONTENT);
+	}
+}
---
+++ VueSpringBootLogin/src/main/java/com/ssafy/vue/model/service/BoardServiceImpl.java
@@ -0,0 +1,50 @@
+package com.ssafy.vue.model.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ssafy.vue.model.mapper.BoardDAO;
+import com.ssafy.vue.model.Board;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+	
+    @Autowired
+	private BoardDAO boardDao;
+
+    @Override
+	public List<Board> retrieveBoard() {
+		return boardDao.selectBoard();
+	}
+    
+  	@Override
+	public boolean writeBoard(Board board) {
+		return boardDao.insertBoard(board) == 1;
+	}
+
+	@Override
+	public Board detailBoard(int no) {
+		return boardDao.selectBoardByNo(no);
+	}
+
+	@Override
+	@Transactional
+	public boolean updateBoard(Board board) {
+		return boardDao.updateBoard(board) == 1;
+	}
+
+	@Override
+	@Transactional
+	public boolean deleteBoard(int no) {
+		return boardDao.deleteBoard(no) == 1;
+	}
+
+	@Override
+	public List<Board> retrieveBoard2(String title) {
+		// TODO Auto-generated method stub
+		return boardDao.selectBoardbyName(title);
+	}
+}
