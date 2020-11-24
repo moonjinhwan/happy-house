3cbd09265b5a4f0a6ccef0b7858ebaf0f4b69054
moonjinhwan <moonjinh123@naver.com>
2020-11-25 00:52:11 +0900

    디테일 수정

--- member-vue/src/components/AptDetail.vue
+++ member-vue/src/components/AptDetail.vue
@@ -4,23 +4,36 @@
       sticky-header
       :items="getAptDetail"
       head-variant="light"
-      @row-clicked="handler"
+      @row-clicked="mypage"
       hover
     ></b-table>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import { mapGetters } from 'vuex';
-
+const SERVER_URL = process.env.VUE_APP_SERVER_URL;
 export default {
   computed: {
     ...mapGetters(['getAptDetail']),
   },
   methods: {
-    handler(object, index) {
-      console.log(object + ' ' + index);
-      alert(this.$store.state.userId);
+    mypage(object) {
+      if (this.$store.state.userId == '') {
+        alert('로그인을 해주세요');
+      } else {
+        axios
+          .post(`${SERVER_URL}/user/mypage`, {
+            object,
+          })
+          .then((response) => {
+            console.log(response.data);
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
     },
   },
 };
