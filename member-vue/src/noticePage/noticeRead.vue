df05e23dfd291c9ee0e5fcb28141e3152439cdd8
skim817 <skim817@aucklanduni.ac.nz>
2020-11-25 18:23:24 +0900

    공지사항 완료

---
+++ member-vue/src/noticePage/noticeRead.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <que />
+    <div id="qna">
+      <detail
+        :content="item.content"
+        :title="item.title"
+        :writer="item.writer"
+        :regtime="item.regtime"
+        :no="item.no"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import http from "@/util/http-common";
+import Detail from "@/components/noticeDetail.vue";
+import que from "@/components/qna.vue";
+
+export default {
+  name: "read",
+  components: {
+    Detail,
+    que,
+  },
+  data: function() {
+    return {
+      item: {},
+    };
+  },
+  created() {
+    http.get(`/notice/${this.$route.query.no}`).then(({ data }) => {
+      this.item = data;
+      console.dir(data);
+    });
+  },
+};
+</script>
+
+<style></style>
